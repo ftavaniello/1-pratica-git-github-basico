01285f795a197efba6addeb35c371045f15987ed
Flávia Carvalho Tavaniello <flatavaniello@gmail.com>
2024-08-23 19:03:00 -0300

    fiz esqueleto do primeiro metodo

--- Testes/src/App.java
+++ Testes/src/App.java
@@ -1,4 +1,10 @@
+import java.util.ArrayList;
+
 public class App {
+    
+    public static int nOcorrencias(ArrayList<Integer> l, Integer el) {
+        return 0;
+    }
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
