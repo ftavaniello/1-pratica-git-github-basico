6a60bca28f006eeb9edf063361421a58ec387bc7
Flávia Carvalho Tavaniello <flatavaniello@gmail.com>
2024-08-26 14:14:55 -0300

    Letra A resolvida + exemplo criado na Main

--- Exercício/src/App.java
+++ Exercício/src/App.java
@@ -3,9 +3,22 @@ import java.util.ArrayList;
 public class App {
     
     public static int nOcorrencias(ArrayList<Integer> l, Integer el) {
-        return 0;
+        int cont = 0;
+        for (Integer i : l) {
+            if (i == el) {
+                cont +=1;
+            }
+        }
+        return cont;
     }
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        ArrayList <Integer> teste = new ArrayList<Integer>();
+        teste.add(1);
+        teste.add(2);
+        teste.add(3);
+        teste.add(3);
+        teste.add (3);
+        teste.add (4);
+        System.out.println ("O número 3 aparece " + nOcorrencias(teste, 3) + " vezes");
     }
 }
