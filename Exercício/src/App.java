449a8fc8d2d2f59d67957185912a88750d7b2a7e
Flávia Carvalho Tavaniello <flatavaniello@gmail.com>
2024-08-26 14:02:43 -0300

    Alterei nome da pasta para Exercício e exclui arquivo teste

---
+++ Exercício/src/App.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class App {
+    
+    public static int nOcorrencias(ArrayList<Integer> l, Integer el) {
+        return 0;
+    }
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
